dd5d03ffb0f01fcd45f3d479b52fef361003e6ac
beefrfr <ben@benjack.co.uk>
2023-05-18 19:15:01 +0100

    [[ images ]] Create image handler

---
+++ images/loadImages.php
@@ -0,0 +1,10 @@
+<?php
+	if (!isset($_GET["id"])) {
+		echo "here";
+		header("Location: /");
+	}
+	echo $_GET["id"];
+	/*
+		In here use the ID to access the images DB, ID will be the image row ID
+	*/
+?>
