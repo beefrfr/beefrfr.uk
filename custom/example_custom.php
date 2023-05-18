85fd2b3c5604810bc9005a9788b2b97f7f667e14
beefrfr <ben@benjack.co.uk>
2023-05-18 23:45:05 +0100

    [[ custom ]] Create custom examples

---
+++ custom/example_custom.php
@@ -0,0 +1,10 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/libraries/requires.php');
+
+	$page = Array(
+		"tabTitle" => "My | Custom Example",
+		"pageTitle" => "Custom Example",
+		"content" => "Custom example content"
+	);
+
+?>
