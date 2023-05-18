ea923d4d926f550f67b212e4d5ab45d40c4093e6
beefrfr <ben@benjack.co.uk>
2023-05-18 19:04:57 +0100

    [[ libraries ]] Create `requires`
    Handles loading all required files and libraries for a script.
    Includes loading `composer` `autoload.php`.

---
+++ libraries/requires.php
@@ -0,0 +1,11 @@
+<?php
+	if (session_status() === PHP_SESSION_NONE) {
+		session_start();
+	}
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/libraries/db.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/libraries/admin/user.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/libraries/admin/adminContentHandler.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/libraries/contentHandler.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/libraries/admin/user.php');
+?>
